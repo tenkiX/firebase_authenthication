6cd70710e3161f9af1f6e6856075b607b58a8f19
tenkiX <36762233+tenkiX@users.noreply.github.com>
2019-12-31 12:52:58 +0100

    initial commit

---
+++ FirebaseManager.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System;
+using Firebase;
+using Firebase.Auth;
+
+public class FirebaseManager : MonoBehaviour
+{
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+    void Start()
+    {
+        // Set auth instance
+        auth = FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void OnDestroy()
+    {
+        if (auth != null)
+        {
+            auth.StateChanged -= AuthStateChanged;
+            auth = null;
+        }
+    }
+
+    // Track state changes of the auth object.
+    void AuthStateChanged(object sender, EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.LogWarning(user.UserId + " is not authenticated.");
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log(user.UserId + " is authenticated.");
+            }
+        }
+    }
+
+    public async void RegisterUserAsync(string email, string password, Action OnSuccess, Action<string> OnError)
+    {
+        AggregateException exception = null;
+        await auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                //if we would call OnError here, we would not be able to use unity main-thread functions there like Instantiate
+                exception = task.Exception;
+            }
+        });
+        if (exception != null)
+        {
+            OnError(GetAuthErrorMessage(exception));
+        }
+        else
+        {
+            OnSuccess();
+        }
+    }
+
+    public async void LogIn(string email, string password, Action OnSuccess, Action<string> OnError)
+    {
+        AggregateException exception = null;
+        await auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                exception = task.Exception;
+            }
+        });
+        if (exception != null)
+        {
+            OnError(GetAuthErrorMessage(exception));
+        }
+        else
+        {
+            OnSuccess();
+        }
+    }
+    
+    public void LogOut()
+    {
+        auth.SignOut();
+        Debug.Log("Logged out");
+    }
+
+    /// <summary> Convert Firebase Authentication error messages to a readable format </summary>
+    string GetAuthErrorMessage(AggregateException exception)
+    {
+        FirebaseException ex = null;
+        foreach (Exception e in exception.Flatten().InnerExceptions)
+        {
+            ex = e as FirebaseException;
+            if (ex != null) { break; }
+        }
+        var message = "";
+        switch ((AuthError)ex.ErrorCode)
+        {
+            case AuthError.AccountExistsWithDifferentCredentials:
+                message = "This username is already taken.";
+                break;
+            case AuthError.MissingPassword:
+                message = "Please enter a password.";
+                break;
+            case AuthError.WeakPassword:
+                message = "The password is too weak (min. 6 character needed).";
+                break;
+            case AuthError.WrongPassword:
+                message = "Wrong password.";
+                break;
+            case AuthError.EmailAlreadyInUse:
+                message = "This username is already taken.";
+                break;
+            case AuthError.InvalidEmail:
+                message = "Invalid username.";
+                break;
+            case AuthError.MissingEmail:
+                message = "Username is missing";
+                break;
+            case AuthError.TooManyRequests:
+                message = "Too many requests";
+                break;
+            case AuthError.Cancelled:
+                message = "Request cancelled.";
+                break;
+            case AuthError.UserNotFound:
+                message = "Username does not exists.";
+                break;
+            case AuthError.QuotaExceeded:
+                message = "Served quota reached for this month. \n Need more money for poor developer to buy better server. :(";
+                break;
+            default:
+                message = "Unhandled error (" + (AuthError)ex.ErrorCode + ")";
+                break;
+        }
+        return message;
+    }
+}
---
+++ LoginTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginTest : MonoBehaviour
+{
+    [SerializeField] FirebaseManager manager;
+    [SerializeField] InputField emailInputField;
+    [SerializeField] InputField passwordInputField;
+
+    #region Register
+    public void OnRegisterButtonClicked()
+    {
+        manager.RegisterUserAsync(emailInputField.text, passwordInputField.text, OnRegistrationSuccess, OnRegistrationFailed);
+    }
+
+    void OnRegistrationSuccess()
+    {
+        Debug.Log("Registration success");
+    }
+
+    void OnRegistrationFailed(string errorMessage)
+    {
+        Debug.LogError(errorMessage);
+    }
+    #endregion
+
+    #region Login
+    public void OnLoginButtonClicked()
+    {
+        manager.LogIn(emailInputField.text, passwordInputField.text, OnLoginSuccess, OnLoginFailed);
+    }
+
+    void OnLoginSuccess()
+    {
+        Debug.Log("Login success");
+    }
+
+    void OnLoginFailed(string errorMessage)
+    {
+        Debug.LogError(errorMessage);
+    }
+    #endregion
+
+    #region Logout
+    public void OnLogoutButtonClicked()
+    {
+        manager.LogOut();
+    }
+    #endregion
+}
+
